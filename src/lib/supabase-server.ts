6b464bb08ad6e50f7de56bb429b3fb16749b85a2
borgesr18 <borgesr18@yahoo.com.br>
2025-06-25 20:40:59 -0300

    Create supabase-server.ts

---
+++ src/lib/supabase-server.ts
@@ -0,0 +1,11 @@
+import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
+
+export const supabaseServer = () => {
+  const cookieStore = cookies();
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies: () => cookieStore }
+  );
+};
