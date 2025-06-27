46931dcbf9d3b37db308d2e1c2e22fafabb77f5b
borgesr18 <borgesr18@yahoo.com.br>
2025-06-27 06:06:24 -0300

    Update supabase-browser.ts

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -2,9 +2,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/types/supabase';
 
-export function supabase() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
